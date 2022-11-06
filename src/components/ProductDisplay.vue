f88837d6f66d33e96106d53a492987e78157155e
pidokige02 <pidokige0204@gmail.com>
2022-11-06 22:18:52 +0900

    5.Dynamic Style

--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -21,7 +21,7 @@ const variants = ref([
   { id: 2234, color: 'green', image: socksGreenImage, quantity: 50 },
   { id: 2235, color: 'blue', image: socksBlueImage, quantity: 0 },
 ])
-    
+
 const title = computed(() => {
   return brand.value + ' ' + product.value
 })
@@ -51,12 +51,14 @@ function updateVariant(index) {
 </script>
 
 <template>
-  <div class="product-display">
-    <div class="product-container">
-      <div class="product-image">
-        <img v-bind:src="image">
+  <div class="p-4">
+    <div class="flex flex-row flex-wrap">
+      <div class="w-[100%] md:w-[50%]">
+        <img class="w-[70%] m-10 p-4 border-2 border-solid border-[#d8d8d8]"
+          v-bind:src="image"
+        />
       </div>
-      <div class="product-info">
+      <div class="w-[100%] md:w-[50%] ml-3 md:ml-0">
         <h1>{{ title }}</h1>
         <p v-if="inStock">In Stock</p>
         <p v-else>Out of Stock</p>
@@ -64,19 +66,24 @@ function updateVariant(index) {
         <ul>
           <li v-for="detail in details">{{ detail }}</li>
         </ul>
-        <div 
-          v-for="(variant, index) in variants" 
-          class="color-circle" 
-          :style="{ backgroundColor: variant.color }"
-          :key="variant.id" 
-          @mouseover="updateVariant(index)" 
+        <div
+          v-for="(variant, index) in variants"
+          class="w-12 h-12 mt-2 border-2 border-solid border-[#d8d8d8] rounded-[50%]"
+          :class="{
+            green: 'bg-[green]',
+            blue: 'bg-[blue]'
+          }[variant.color]"
+          :key="variant.id"
+          @mouseover="updateVariant(index)"
         >
         </div>
-        
-        <button 
-          class="button" 
-          :class="{ disabled: !inStock }" 
-          :disabled="!inStock" 
+
+        <button
+          class="w-40 h-16 m-8 p-5 text-lg leading-none text-white text-center rounded-[5px] btn-shading-bn"
+          :class="inStock ?
+            ['bg-[#39495c]', 'cursor-pointer'] :
+            ['bg-[#d8d8d8]', 'cursor-not-allowed']"
+          :disabled="!inStock"
           @click="addToCart"
         >
           Add to Cart
@@ -87,41 +94,34 @@ function updateVariant(index) {
 </template>
 
 <style scoped>
-
-.product-display {
+/* .product-display {
   padding: 16px;
 }
-
 .product-container {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
 }
-
 .product-image {
   width: 100%;
 }
-
 img {
   width: 70%;
   margin: 40px;
   padding: 15px;
   border: 2px solid #d8d8d8;
 }
-
 .product-info {
   width: 100%;
   margin-left: 10px;
-}
-
-.color-circle {
+} */
+/* .color-circle {
   width: 50px;
   height: 50px;
   margin-top: 8px;
   border: 2px solid #d8d8d8;
   border-radius: 50%;
 }
-
 .button {
   width: 160px;
   height: 60px;
@@ -131,28 +131,20 @@ img {
   color: white;
   text-align: center;
   border-radius: 5px;
-  box-shadow: inset 0 -0.6em 1em -0.35em rgba(0, 0, 0, 0.17),
-    inset 0 0.6em 2em -0.3em rgba(255, 255, 255, 0.15),
-    inset 0 0 0em 0.05em rgba(255, 255, 255, 0.12);
-  
   background-color: #39495c;
   cursor: pointer;
-}
-
-.button.disabled {
+} */
+/* .button.disabled {
   background-color: #d8d8d8;
   cursor: not-allowed;
-}
-
-@media only screen and (min-width: 860px) {
-
+} */
+/* @media only screen and (min-width: 860px) {
   .product-image {
     width: 50%;
   }
-
   .product-info {
     width: 50%;
     margin-left: 0;
   }
-} 
+}  */
 </style>
